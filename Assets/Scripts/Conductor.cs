944f68994a2d2962c10fe18204e78d09b8543475
Michael Gallagher <navysmeagol0175@gmail.com>
2022-02-22 11:43:52 -0500

    Barebones conductor class based on tutorial

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coductor : MonoBehaviour
+public class Conductor : MonoBehaviour
 {
+    //the number of beats in each loop
+    public float beatsPerLoop;
+
+    //the total number of loops completed since the looping clip first started
+    public int completedLoops = 0;
+
+    //The current position of the song within the loop in beats.
+    public float loopPositionInBeats;
+
+    //The current relative position of the song within the loop measured between 0 and 1.
+    public float loopPositionInAnalog;
+
+    //Song beats per minute
+    //This is determined by the song you're trying to sync up to
+    public float songBpm;
+
+    //The number of seconds for each song beat
+    public float secPerBeat;
+
+    //Current song position, in seconds
+    public float songPosition;
+
+    //Current song position, in beats
+    public float songPositionInBeats;
+
+    //The offset to the first beat of the song in seconds
+    public float firstBeatOffset;
+
+    //How many seconds have passed since the song started
+    public float dspSongTime;
+
+    //an AudioSource attached to this GameObject that will play the music.
+    public AudioSource musicSource;
+
+
+    //Conductor instance
+    public static Conductor instance;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Load the AudioSource attached to the Conductor GameObject
+        musicSource = GetComponent<AudioSource>();
+
+        //Calculate the number of seconds in each beat
+        secPerBeat = 60f / songBpm;
+
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start the music
+        musicSource.Play();
+    }
+
+    void Awake()
+    {
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //calculate the loop position
+        if (songPositionInBeats >= (completedLoops + 1) * beatsPerLoop)
+            completedLoops++;
+        loopPositionInBeats = songPositionInBeats - completedLoops * beatsPerLoop;
+
+        loopPositionInAnalog = loopPositionInBeats / beatsPerLoop;
+
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
     }
 }
