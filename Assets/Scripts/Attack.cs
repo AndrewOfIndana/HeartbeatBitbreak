b4f82f981378ebe2d5cab4cab7aff3b956baa4af
Michael Gallagher <navysmeagol0175@gmail.com>
2022-03-05 19:50:38 -0500

    Implemented Attack class

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+
+public class Attack {
+
+    protected int Damage;
+    protected int ToHit;
+
+    public Attack(int Damage, int ToHit) {
+        //Takes the values and initializes the attributes
+        this.Damage = Damage;
+        this.ToHit = ToHit;         
+    }
+
+    public int GetDamage() {
+        //Getter for Damage attribute
+        return this.Damage;
+    }
+
+    public int GetToHit() {
+        //Getter for ToHitAttribute
+        return this.ToHit;
+    }
+}
