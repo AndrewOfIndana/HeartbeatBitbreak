1867ffc511220fe0f96a9d1a49c3943b77cc23d7
Michael Gallagher <navysmeagol0175@gmail.com>
2022-03-05 20:55:20 -0500

    implented player character abstract methods

--- Assets/Scripts/ScriptableObjects/PlayerCharacter.cs
+++ Assets/Scripts/ScriptableObjects/PlayerCharacter.cs
@@ -1,13 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[CreateAssetMenu(fileName = "PlayerCharacter", menuName = "ScriptableObjects/PlayerDataClass", order = 0)]
 public class PlayerCharacter : GenericCreature
 {
     public override void ReceiveAttack(Attack attack)
     {
-        throw new System.NotImplementedException();
+        if (attack.GetToHit() >= this.def) {
+
+            this.health -= attack.GetDamage();
+
+            if (this.health < 0) {
+                health = 0;
+            }
+        }
     }
 
     public override Attack GetAttack()
     {
-        throw new System.NotImplementedException();
+        int damage = this.atk;
+        int tohit = 20; 
+
+        return new Attack(damage, tohit);
     }
 }
