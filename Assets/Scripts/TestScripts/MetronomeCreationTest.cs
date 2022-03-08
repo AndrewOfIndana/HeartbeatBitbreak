c8f33047b348d444a3ae5983679511e7c6564acc
Michael Gallagher <navysmeagol0175@gmail.com>
2022-03-08 14:37:41 -0500

    Simple test for duplicating children and joining their position

--- Assets/Scripts/TestScripts/MetronomeCreationTest.cs
+++ Assets/Scripts/TestScripts/MetronomeCreationTest.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MetronomeCreationTest : MonoBehaviour
 {
+    public GameObject child;
+    public float moveamount = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject child2 = GameObject.Instantiate(child);
+        child2.transform.parent = gameObject.transform;
+        child2.transform.Translate(Vector3.right * moveamount);
     }
 
     // Update is called once per frame
