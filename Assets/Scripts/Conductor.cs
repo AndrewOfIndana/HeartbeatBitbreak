984aec7c09c2b6cb8f680ef8ad260887fae2d09d
Michael Gallagher <navysmeagol0175@gmail.com>
2022-03-12 18:52:45 -0500

    Implemented SyncBeat in Conductor.cs

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Conductor : MonoBehaviour
+public class Conductor : MonoBehaviour, SyncBeat
 {
     //the number of beats in each loop
     public float beatsPerLoop;
@@ -56,10 +56,10 @@ public class Conductor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.StartLoop();
+        //this.StartLoop();
     }
 
-    public void StartLoop() {
+    public void StartBeat() {
         //Load the AudioSource attached to the Conductor GameObject
         musicSource = GetComponent<AudioSource>();
 
