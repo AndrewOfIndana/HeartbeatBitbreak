83ca8eb84a3c9772059731a7d5978789c9a2d031
Michael Gallagher <navysmeagol0175@gmail.com>
2022-03-18 22:01:17 -0400

    GetAttack has a chance to crit

--- Assets/Scripts/ScriptableObjects/EnemyCharacter.cs
+++ Assets/Scripts/ScriptableObjects/EnemyCharacter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/EnemyDataClass", order = 1)]
 public class EnemyCharacter : GenericCreature
 {
-
+    public int CritChance = 30;
     public override void ReceiveAttack(Attack attack) {
 
         this.health -= attack.GetDamage();
@@ -20,8 +20,11 @@ public class EnemyCharacter : GenericCreature
 
     public override Attack GetAttack()
     {
-        
-        int damage = this.atk;
+        int damage_mult = 1;
+        if (Random.Range(0, 100) <= this.CritChance) {
+            damage_mult = 2;        
+        }
+        int damage = this.atk * damage_mult;
         int tohit = Random.Range(0, 20); // Generates a random int between 0-19
 
         return new Attack(damage, tohit);
